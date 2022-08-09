2e6f8cbbbec63382d306cc9aa3074e2a3d2cca42
GAURAV5925 <jhagaurav5925@gmail.com>
2022-08-09 22:22:49 +0530

    React Project

---
+++ todos-list/src/MyComponent/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>This is an About Component
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, possimus magnam. Placeat similique ex dolore, nam eligendi, tempora quaerat suscipit voluptatum quo consequuntur recusandae.</p>
+    </div>
+  )
+}
